0d91986471d4d400953c62d93c2c4a265cb49448
VSETTER6 <vilhelm.settergren@live.se>
2024-11-15 14:13:16 +0100

    Renamed class

---
+++ tdd-ovning-2/TrängselskattTest/KalkylatorTests.cs
@@ -0,0 +1,15 @@
+﻿namespace TrängselskattTest;
+
+public class KalkylatorTests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test()
+    {
+        Assert.Pass();
+    }
+}
